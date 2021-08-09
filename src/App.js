eabefb6cc8af0c5c40152cdb9ce9dbb58419a9b7
G Chao <81912588+powerlesbian@users.noreply.github.com>
2021-08-09 11:33:22 +0800

    initial fork from Ant's repo with updated API key

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Gif from './components/Gif';
+import SearchGif from './components/SearchGif';
+
+
 
 function App() {
+  const [gifs, setGifs] = useState([]);
+  const [favoriteGifs, setFavoriteGifs] = useState([]);
+
+  useEffect(() => {
+    const getTrendingGifs = async () => {
+      const response = await axios.get('https://api.giphy.com/v1/gifs/trending?api_key=EXcg3N1MInQC4wDdCETkGmUQNT3wouoO');
+      setGifs(response.data.data);
+    }
+    getTrendingGifs();
+  }, []);
+
+  const searchGifs = async (searchText) => {
+    const response = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=r9lryrT1sb53mewKob2973KLcJiNh1bO&q=${searchText}`);
+    setGifs(response.data.data);
+  }
+
+  const saveFavorite = (gifId) => {
+    const favGif = gifs.find(gif => gif.id === gifId);
+    setFavoriteGifs([...favoriteGifs, favGif]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="App">
+      <header className="App-header"></header>
+      <h1>Showing you cool free stuff that we got from other people!</h1>
+      <SearchGif searchGifs={searchGifs} />
+      <p>Favorite gifs</p>
+      {favoriteGifs.map((gif) => (
+        <Gif
+          key={gif.id}
+          imageUrl={gif.images.fixed_height.url}
+          title={gif.images.title} />
+      ))}
+      <p>Trending or searched gifs</p>
+      {gifs.map((gif) => (
+        <Gif
+          key={gif.id}
+          imageUrl={gif.images.fixed_height.url}
+          title={gif.images.title}
+          id={gif.id}
+          saveFavorite={saveFavorite} />
+      ))}
+      {/* component to capture the search form
+      component to render all the trending gifs
+      component to render the favorite gifs */}
     </div>
   );
 }
-
 export default App;
