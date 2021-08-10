0934b2d821343f2c42b57e04abf49aa18852c4a4
G Chao <81912588+powerlesbian@users.noreply.github.com>
2021-08-10 16:48:24 +0800

    renders out a list of 151 pokemon names and links

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import Gif from './components/Gif';
-import SearchGif from './components/SearchGif';
+// import Gif from './components/Gif';
+// import SearchGif from './components/SearchGif';
 
 
 
@@ -11,46 +11,44 @@ function App() {
 
   useEffect(() => {
     const getTrendingGifs = async () => {
-      const response = await axios.get('https://api.giphy.com/v1/gifs/trending?api_key=EXcg3N1MInQC4wDdCETkGmUQNT3wouoO');
-      setGifs(response.data.data);
+      const response = await axios.get('https://pokeapi.co/api/v2/pokemon/?limit=151');
+      setGifs(response.data.results);
+      console.log(response.data.results);
     }
     getTrendingGifs();
   }, []);
 
-  const searchGifs = async (searchText) => {
-    const response = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=EXcg3N1MInQC4wDdCETkGmUQNT3wouoO&q=${searchText}`);
-    setGifs(response.data.data);
-  }
+  // const searchGifs = async (searchText) => {
+  //   const response = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=EXcg3N1MInQC4wDdCETkGmUQNT3wouoO&q=${searchText}`);
+  //   setGifs(response.data.data);
+  // }
 
-  const saveFavorite = (gifId) => {
-    const favGif = gifs.find(gif => gif.id === gifId);
-    setFavoriteGifs([...favoriteGifs, favGif]);
-  }
+  // const saveFavorite = (gifId) => {
+  //   const favGif = gifs.find(gif => gif.id === gifId);
+  //   setFavoriteGifs([...favoriteGifs, favGif]);
+  // }
 
   return (
         <div className="App">
       <header className="App-header"></header>
       <h1>Showing you cool free stuff that we got from other people!</h1>
-      <SearchGif searchGifs={searchGifs} />
+      {/* <SearchGif searchGifs={searchGifs} />
       <p>Favorite gifs</p>
       {favoriteGifs.map((gif) => (
         <Gif
           key={gif.id}
           imageUrl={gif.images.fixed_height.url}
           title={gif.images.title} />
+      ))} */}
+      <p>Flavour of the day is.... Pokemon! 👻👾</p>
+      {gifs.map((gif,index) => (
+        <li key={index}>
+          {index+1}. Pocket Monster:  {gif.name}, 
+          <a href={gif.url}>     click for more info on {gif.name}</a> <br></br>
+          </li>
+
       ))}
-      <p>Trending or searched gifs</p>
-      {gifs.map((gif) => (
-        <Gif
-          key={gif.id}
-          imageUrl={gif.images.fixed_height.url}
-          title={gif.images.title}
-          id={gif.id}
-          saveFavorite={saveFavorite} />
-      ))}
-      {/* component to capture the search form
-      component to render all the trending gifs
-      component to render the favorite gifs */}
+
     </div>
   );
 }
